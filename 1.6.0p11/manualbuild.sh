4763e12b5e12cecd823afbbaa5269812f411ebe7
Igor Kov <igor875126@gmail.com>
2021-02-11 15:52:23 +0600

    Initial commit

---
+++ 1.6.0p11/manualbuild.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+###########################################################################
+# Builds docker image and pushes as latest to the registry
+# Normally this is made by CI/CD automatically but in case it does not work
+###########################################################################
+
+# Variables
+TAG=1.6.0p11
+REPOSITORY=igor875126/checkmk
+IMAGENAME=${REPOSITORY}:${TAG}
+
+# Build
+docker build -t ${IMAGENAME} .
+
+# Push
+docker push ${IMAGENAME}
+
+# Cleanup
+YELLOW='\033[0;33m'
+NC='\033[0m'
+echo -e "To clean up local image, type: ${YELLOW}docker rmi ${IMAGENAME}${NC}"
